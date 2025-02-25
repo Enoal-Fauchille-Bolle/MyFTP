40ab4ede857e716a227b06ae8513e43a77f3be90
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-02-25 15:01:42 +0100

    feat: implement user command handling to manage user login state

--- src/commands/user.c
+++ src/commands/user.c
@@ -9,7 +9,19 @@
 
 command_status_t user_command(command_t *command, connection_t *connection)
 {
-    (void)command;
-    (void)connection;
+    if (connection->logged_in) {
+        dprintf(connection->client_sockfd,
+            "530 Can't change to another user.\r\n");
+        return COMMAND_FAILURE;
+    }
+    // printf("user: %s\n", connection->user);
+    if (connection->user)
+        free(connection->user);
+    if (command->argc == 0) {
+        connection->user = NULL;
+    } else {
+        connection->user = strdup(command->argv[0]);
+    }
+    dprintf(connection->client_sockfd, "331 Please specify the password.\r\n");
     return COMMAND_SUCCESS;
 }
