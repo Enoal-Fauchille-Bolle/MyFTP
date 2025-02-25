5088c241859fa90c15916b8c4026722dfcd72d6c
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-02-25 11:18:18 +0100

    feat: implement help command to display recognized FTP commands

--- src/commands/help.c
+++ src/commands/help.c
@@ -10,6 +10,11 @@
 command_status_t help_command(command_t *command, connection_t *connection)
 {
     (void)command;
-    (void)connection;
+    dprintf(connection->client_sockfd,
+        "214-The following commands are recognized.\r\n");
+    dprintf(connection->client_sockfd,
+        "USER PASS CWD CDUP QUIT DELE PWD PASV PORT HELP NOOP RETR STOR "
+        "LIST\r\n");
+    dprintf(connection->client_sockfd, "214 Help OK.\r\n");
     return COMMAND_SUCCESS;
 }
