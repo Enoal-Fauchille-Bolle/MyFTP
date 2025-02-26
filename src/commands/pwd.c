8ac548c7ed5d2be6ecc696cb810ccce437d029ee
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-02-26 11:28:00 +0100

    feat: add response for PWD command to return current working directory

--- src/commands/pwd.c
+++ src/commands/pwd.c
@@ -15,5 +15,7 @@ command_status_t pwd_command(command_t *command, connection_t *connection)
             "530 Please login with USER and PASS.\r\n");
         return COMMAND_FAILURE;
     }
+    dprintf(connection->client_sockfd, "257 \"%s\"\r\n",
+        connection->working_directory);
     return COMMAND_SUCCESS;
 }
