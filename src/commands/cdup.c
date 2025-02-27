062532222cf6ea510807d672da6d00145bc4043f
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-02-27 15:52:39 +0100

    feat: implement directory change handling in cdup command

--- src/commands/cdup.c
+++ src/commands/cdup.c
@@ -7,6 +7,57 @@
 
 #include "myftp.h"
 
+static int check_directory(connection_t *connection, DIR *directory)
+{
+    if (directory != NULL) {
+        closedir(directory);
+    } else {
+        dprintf(
+            connection->client_sockfd, "550 Failed to change directory.\r\n");
+        return 1;
+    }
+    return 0;
+}
+
+static int check_exists(connection_t *connection, char *path)
+{
+    if (access(path, F_OK) != 0) {
+        dprintf(
+            connection->client_sockfd, "550 Failed to change directory.\r\n");
+        return 1;
+    }
+    return 0;
+}
+
+static void check_new_path(connection_t *connection)
+{
+    if (strncmp(connection->working_directory, connection->server->path,
+            strlen(connection->server->path)) != 0) {
+        free(connection->working_directory);
+        connection->working_directory = getcwd(NULL, 0);
+    }
+}
+
+static command_status_t change_dir(connection_t *connection, char *path)
+{
+    DIR *directory = NULL;
+
+    if (check_exists(connection, path) == 1)
+        return COMMAND_FAILURE;
+    directory = opendir(path);
+    if (check_directory(connection, directory) == 1)
+        return COMMAND_FAILURE;
+    chdir(connection->working_directory);
+    chdir(path);
+    free(connection->working_directory);
+    connection->working_directory = getcwd(NULL, 0);
+    chdir(connection->server->path);
+    check_new_path(connection);
+    dprintf(
+        connection->client_sockfd, "250 Directory successfully changed.\r\n");
+    return COMMAND_SUCCESS;
+}
+
 command_status_t cdup_command(command_t *command, connection_t *connection)
 {
     (void)command;
@@ -15,6 +66,5 @@ command_status_t cdup_command(command_t *command, connection_t *connection)
             "530 Please login with USER and PASS.\r\n");
         return COMMAND_FAILURE;
     }
-    dprintf(connection->client_sockfd, "502 Command not implemented.\r\n");
-    return COMMAND_SUCCESS;
+    return change_dir(connection, "..");
 }
