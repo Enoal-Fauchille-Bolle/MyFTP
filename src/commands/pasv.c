252c2f12a73b7211b4ac4b662c184c71cbfc9b22
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-02-28 18:20:41 +0100

    feat: implement data socket creation in pasv command

--- src/commands/pasv.c
+++ src/commands/pasv.c
@@ -7,6 +7,27 @@
 
 #include "myftp.h"
 
+static void data_socket(connection_t *connection)
+{
+    data_socket_t *data_socket = malloc(sizeof(data_socket_t));
+    int *ports = NULL;
+
+    if (data_socket == NULL) {
+        dprintf(connection->client_sockfd,
+            "451 Requested action aborted: local error in processing.\r\n");
+        return;
+    }
+    *data_socket = setup_data_socket();
+    printf("Data socket created on port %d\n", data_socket->port);
+    ports = split_port(data_socket->port);
+    dprintf(connection->client_sockfd,
+        "227 Entering Passive Mode (%s,%d,%d).\r\n",
+        replace_dots_with_commas(inet_ntoa(data_socket->addr.sin_addr)),
+        ports[0], ports[1]);
+    free(ports);
+    connection->data_socket = data_socket;
+}
+
 command_status_t pasv_command(command_t *command, connection_t *connection)
 {
     (void)command;
@@ -15,6 +36,6 @@ command_status_t pasv_command(command_t *command, connection_t *connection)
             "530 Please login with USER and PASS.\r\n");
         return COMMAND_FAILURE;
     }
-    dprintf(connection->client_sockfd, "502 Command not implemented.\r\n");
+    data_socket(connection);
     return COMMAND_SUCCESS;
 }
