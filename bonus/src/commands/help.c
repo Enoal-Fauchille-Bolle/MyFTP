ca1c0552e756ce2214b1cb1c56f5b83335521918
Enoal Fauchille-Bolle <enoal.fauchille-bolle@epitech.eu>
2025-03-12 15:59:48 +0100

    feat: add help command for user guidance
    added a new help command that lists available commands.
    includes basic structure and prints out the command options to assist users.

---
+++ bonus/src/commands/help.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** MyFTP
+** File description:
+** HELP Command
+*/
+
+#include "myftp.h"
+
+command_status_t help_command(command_t *command, client_t *client)
+{
+    (void)command;
+    (void)client;
+    printf("Available commands:\n");
+    printf("HELP QUIT EXIT LS PWD CD GET PUT\n");
+    return COMMAND_QUIT;
+}
